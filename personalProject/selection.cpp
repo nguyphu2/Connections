06abe192ac3c74f5319d1acdcbaa5df2d92c8dce
nguyphu2 <165837233+nguyphu2@users.noreply.github.com>
2024-10-12 19:41:36 -0700

    adding selection method

--- personalProject/selection.cpp
+++ personalProject/selection.cpp
@@ -1,1 +1,29 @@
 #include "selection.h"
+
+void Selection::addPlayer(const Player &player){
+    sortedTraits["college"][player.getCollege()].push_back(player);
+    sortedTraits["age"][to_string(player.getAge())].push_back(player);
+    sortedTraits["height"][to_string(player.getHeight())].push_back(player);
+    sortedTraits["yearsPro"][to_string(player.getYearsPro())].push_back(player);
+    sortedTraits["position"][player.getPosition()].push_back(player);
+    sortedTraits["sideOfBall"][player.getSideOfBall()].push_back(player);
+    sortedTraits["weight"][to_string(player.getWeight())].push_back(player);
+    sortedTraits["jerseyNumber"][to_string(player.getJerseyNumber())].push_back(player);
+    sortedTraits["fact"][player.getFact()].push_back(player);
+}
+
+vector<Player> Selection::getPlayersByTrait (const string &trait, const string &specificTrait){
+    if (sortedTraits.find(trait)!=sortedTraits.end() && sortedTraits[trait].find(specificTrait) != sortedTraits[trait].end()){
+        return sortedTraits[trait][specificTrait];
+    }
+    else{
+        return {};
+    }
+}
+
+string Selection::getEasyTrait(const vector<string>& easyTraits){
+    
+};
+string Selection::getIntermediateTrait(const vector<string>& intermediateTraits){}; 
+string Selection::getMediumTrait(const vector<string>& mediumTraits){};
+string Selection::getHardTrait(const vector<string>& hardTraits){};
--- personalProject/selection.h
+++ personalProject/selection.h
@@ -1,12 +1,34 @@
 #ifndef SELECTION_H
 #define SELECTION_H
+#include "players.h"
+#include<string>
+#include<unordered_map>
+#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
 
 class Selection {
     private:
     
-    public:
+    unordered_map<string, unordered_map<string, vector<Player>>>sortedTraits;
+    vector<string> easyTraits = {"position", "age", "sideOfBall"};
+    vector<string> intermediateTraits = {"jerseyNumber", "yearsPro"};
+    vector<string> mediumTraits = {"height", "college"};
+    vector<string> hardTraits = {"weight", "fact"};
+    
+    public: 
 
+    void addPlayer(const Player &player);
 
+    vector<Player> getPlayersByTrait (const string &trait, const string &specificTrait);
+
+    string getEasyTrait(const vector<string>& easyTraits);
+    string getIntermediateTrait(const vector<string>& intermediateTraits); 
+    string getMediumTrait(const vector<string>& mediumTraits);
+    string getHardTrait(const vector<string>& hardTraits);
 };
 
 
